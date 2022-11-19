f443611711378e0f6e80c5a575ff0930172b6428
elis-11 <eliza.arzanukaeva@gmail.com>
2022-11-19 21:00:39 +0100

    Cart-already-Exist-product

---
+++ cart-js/cart-exist-03.js
@@ -0,0 +1,76 @@
+//2. parent of cart
+const cartWrapper = document.querySelector(".cart-wrapper");
+
+//1. search click on the window
+window.addEventListener("click", (event) => {
+  // check if clicked the button 'buy now'
+  if (event.target.hasAttribute("data-cart")) {
+    // console.log(" clicked on button");
+
+    // find clicked card     // CLOSEST - find parent of the card
+    const card = event.target.closest(".card");
+    console.log(card);
+
+    // collect all info about the card & write together
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector(".product-img").getAttribute("src"),
+      title: card.querySelector(".item-title").innerHTML,
+      itemInBox: card.querySelector("[data-items-in-box]").innerHTML,
+      weight: card.querySelector(".price__weight").innerHTML,
+      price: card.querySelector(".price__currency").innerHTML,
+      counter: card.querySelector("[data-counter]").innerHTML,
+    };
+    console.log(productInfo);
+
+    //! 03 check if there is such a product in cart
+    const itemInCart = cartWrapper.querySelector(
+      `[data-id="${productInfo.id}"]`
+    );
+
+    //! 03 if product is in cart
+    if (itemInCart) {
+      const counterElem = itemInCart.querySelector("[data-counter]");
+      counterElem.innerHTML =
+        parseInt(counterElem.innerHTML) + parseInt(productInfo.counter);
+    } else {
+      //! 03 if product is not in cart
+      // put all collected data
+      const cartItemHTML = `
+    <div class="cart-item" data-id="${productInfo.id}">
+    <div class="cart-item__top">
+      <div class="cart-item__img">
+        <img src="${productInfo.imgSrc}" alt="" />
+      </div>
+      <div class="cart-item__desc">
+        <div class="cart-item__title">${productInfo.title}</div>
+        <div class="cart-item__weight">${productInfo.itemInBox} / ${productInfo.weight}</div>
+        <!-- <div class="cart-item__weight">6 st. / 205g.</div> -->
+
+        <!-- cart-item__details -->
+        <div class="cart-item__details">
+          <div class="items items--small counter-wrapper">
+            <div class="items__control" data-action="minus">
+              -
+            </div>
+            <div class="items__current" data-counter="">${productInfo.counter}</div>
+            <div class="items__control" data-action="plus">+</div>
+          </div>
+
+          <div class="price">
+            <div class="price__currency">${productInfo.price}</div>
+          </div>
+        </div>
+        <!-- // cart-item__details -->
+      </div>
+    </div>
+  </div>
+    `;
+      // show products in cart
+      cartWrapper.insertAdjacentHTML("beforeend", cartItemHTML);
+    }
+
+    //! 03 clear counter
+    card.querySelector("[data-counter]").innerHTML = "1";
+  }
+});
