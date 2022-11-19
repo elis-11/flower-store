53ac5db0496b24f467ba678c8ff41afa7e14c3ce
elis-11 <eliza.arzanukaeva@gmail.com>
2022-11-19 22:05:45 +0100

    Counter:  Delete product from Cart - 4

---
+++ counter-js/counter-delete-prod-4.js
@@ -0,0 +1,35 @@
+// add listener on the entire window - na vsöm okne
+window.addEventListener("click", (event) => {
+  let counter; // declare a variable for the counter
+
+  // check if click passed to 'plus' or 'minus'
+  if (
+    event.target.dataset.action === "plus" ||
+    event.target.dataset.action === "minus"
+  ) {
+    const counterWrapper = event.target.closest(".counter-wrapper"); // find parent of the clicked-element
+    counter = counterWrapper.querySelector("[data-counter]"); // find data-counter
+  }
+
+  // event-z.2  target-zel  dataset-all buttons plus action-click
+
+  //! check if the element is button Plus
+  if (event.target.dataset.action === "plus") {
+    counter.innerHTML = ++counter.innerHTML; // add 1 item to the cart
+  }
+
+  //! check if the element is button Minus
+  if (event.target.dataset.action === "minus") {
+    if (parseInt(counter.innerHTML) > 1) {
+      counter.innerHTML = --counter.innerHTML; // - 1 item to the cart
+
+      //! 04 check prod in cart
+    } else if (
+      event.target.closest(".cart-wrapper") && parseInt(counter.innerHTML) === 1
+    ) {
+      console.log(" in cart");
+      //! 04 delete prod
+      event.target.closest(".cart-item").remove();
+    }
+  }
+});
